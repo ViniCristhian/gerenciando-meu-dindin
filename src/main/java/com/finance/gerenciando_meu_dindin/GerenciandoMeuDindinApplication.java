e289eef38237bc854d1d6e4534172686727f49bc
ViniCristhian <viniciuscristhian34@gmail.com>
2025-12-12 12:34:26 -0300

    criação projeto, dependencias que seram  utilizadas e estrutura de pastas

---
+++ src/main/java/com/finance/gerenciando_meu_dindin/GerenciandoMeuDindinApplication.java
@@ -0,0 +1,13 @@
+package com.finance.gerenciando_meu_dindin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciandoMeuDindinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciandoMeuDindinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finance/gerenciando_meu_dindin/GerenciandoMeuDindinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.gerenciando_meu_dindin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciandoMeuDindinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
